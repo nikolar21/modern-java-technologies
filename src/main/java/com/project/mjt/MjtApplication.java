36d061497917f9a37b6ee74c1a7b444e084a6004
Stefan Stanisev <shadowrageinc@gmail.com>
2022-10-25 19:50:50 +0300

    Initial setup commit

---
+++ src/main/java/com/project/mjt/MjtApplication.java
@@ -0,0 +1,13 @@
+package com.project.mjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/mjt/MjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
