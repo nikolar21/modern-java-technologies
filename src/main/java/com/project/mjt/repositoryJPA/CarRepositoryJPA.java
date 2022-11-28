7ffaf54c2765446e30c26eb2e817b61de8ba7d27
FilipDakov <filip_dakov@abv.bg>
2022-11-28 22:31:23 +0200

    added slides in presentation, removed fileRepository

--- src/main/java/com/project/mjt/repositoryJPA/CarRepositoryJPA.java
+++ src/main/java/com/project/mjt/repositoryJPA/CarRepositoryJPA.java
@@ -2,6 +2,8 @@ package com.project.mjt.repositoryJPA;
 
 import com.project.mjt.models.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepositoryJPA extends JpaRepository<Car,Long> {
 }
